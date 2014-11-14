d7b8c1280b0897e034bf6714a15671fc5fd655ae
Tyler Yates <tyler.yates@utexas.edu>
2014-11-14 12:52:57 -0600

    Add strict matcher class and test

---
+++ src/main/java/com/jmatch/StrictSingleVariablePatternMatcher.java
@@ -0,0 +1,77 @@
+package com.jmatch;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * {@link com.jmatch.Matcher} implementation that uses single letters to represent variables in patterns.
+ * <p>
+ * The pattern matching for this class is strict such that different variables are <b>not</b> allowed to share the same
+ * value. For example, the pattern {@code "xyyx"} would not match {@code "aaaa"} because both {@code 'x'} and {@code
+ * 'y'} would have to be assigned the value {@code "a"}.
+ */
+public class StrictSingleVariablePatternMatcher implements Matcher {
+    private final String pattern;
+
+    /**
+     * Constructs a new strict matcher with the given pattern. The pattern should consist of single letters to represent
+     * variables in the pattern. E.g. {@code "abba"} is composed of the variables {@code 'a'} and {@code 'b'}.
+     *
+     * @param pattern the given pattern
+     */
+    public StrictSingleVariablePatternMatcher(String pattern) {
+        this.pattern = pattern;
+    }
+
+    @Override
+    public boolean matches(String input) {
+        return matchRemaining(pattern, input, new HashSet<>());
+    }
+
+    private boolean matchRemaining(String pattern, String input, Set<String> assignments) {
+        // If there is no more of the pattern left to match we need an empty input String to have matched the
+        // original input.
+        if (pattern.isEmpty()) {
+            return input.isEmpty();
+        }
+
+        // Pick the first letter in the pattern as our current variable
+        final String variable = Character.toString(pattern.charAt(0));
+        // Count the number of times the variable occurs in the pattern
+        final int numberOfVariableOccurrencesInPattern = StringUtils.countMatches(pattern, variable);
+
+        // Greedily attempt to assign a value to the current variable
+        for (int endIndex = input.length(); endIndex > 0; endIndex--) {
+            final String variableAssignment = input.substring(0, endIndex);
+
+            // Ensure that different variables do not have the same assigned value
+            if (assignments.contains(variableAssignment)) {
+                continue;
+            }
+
+            assignments.add(variableAssignment);
+            // Check to make sure that the proper number of occurrences of the assigned value exist in the input
+            final int numberOfAssignmentOccurrencesInInput = StringUtils.countMatches(input, variableAssignment);
+            if (numberOfAssignmentOccurrencesInInput >= numberOfVariableOccurrencesInPattern) {
+                // Get the permutations of removing the assignment the same number of times as the variable occurred
+                // in the input
+                final List<String> removalPermutations = StringRemover.getRemovalPermutations(variableAssignment,
+                        numberOfVariableOccurrencesInPattern, input);
+
+                // Check to see if the assignment leads to a valid match
+                for (String removalPermutation : removalPermutations) {
+
+                    if (matchRemaining(pattern.replaceAll(variable, ""), removalPermutation, assignments)) {
+                        return true;
+                    }
+                }
+            }
+            assignments.remove(variableAssignment);
+        }
+
+        return false;
+    }
+}
---
+++ src/test/java/com/jmatch/StrictSingleVariablePatternMatcherTest.java
@@ -0,0 +1,31 @@
+package com.jmatch;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+@SuppressWarnings("JavaDoc")
+public class StrictSingleVariablePatternMatcherTest {
+
+    @Test
+    public void testMatchesValid() {
+        final StrictSingleVariablePatternMatcher matcher = new StrictSingleVariablePatternMatcher("abba");
+        assertTrue(matcher.matches("dogcatcatdog"));
+        assertTrue(matcher.matches("HelloGeorgeByeByeHelloGeorge"));
+        assertTrue(matcher.matches("abba"));
+        assertTrue(matcher.matches("abbbba"));
+        assertTrue(matcher.matches("baab"));
+        assertTrue(matcher.matches("aaaaaa"));
+    }
+
+    @Test
+    public void testMatchesInvalid() {
+        final StrictSingleVariablePatternMatcher matcher = new StrictSingleVariablePatternMatcher("abba");
+        assertFalse(matcher.matches("dogcat"));
+        assertFalse(matcher.matches("abbba"));
+        assertFalse(matcher.matches(""));
+        assertFalse(matcher.matches("aaaa"));
+        assertFalse(matcher.matches("aaaaa"));
+    }
+}
