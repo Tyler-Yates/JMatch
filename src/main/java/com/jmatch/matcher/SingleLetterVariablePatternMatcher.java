623adbc2ff1cb83658dd3ae70f10068bd1214773
Tyler Yates <tyler.yates@utexas.edu>
2014-11-18 17:30:33 -0600

    Refactor code to reduce redundancy between strict and non-strict classes

--- src/main/java/com/jmatch/matcher/SingleLetterVariablePatternMatcher.java
+++ src/main/java/com/jmatch/matcher/SingleLetterVariablePatternMatcher.java
@@ -1,10 +1,6 @@
 package com.jmatch.matcher;
 
-import com.jmatch.util.StringRemover;
-import org.apache.commons.lang3.StringUtils;
-
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -13,7 +9,7 @@ import java.util.Map;
  * The pattern matching for this class is non-strict such that different variables are allowed to share the same value.
  * For example, the pattern {@code "xyyx"} would match {@code "aaaa"} by assigning {@code "a"} to both variables.
  */
-public class SingleLetterVariablePatternMatcher implements VariablePatternMatcher {
+public class SingleLetterVariablePatternMatcher extends LetterVariablePatternMatcher implements VariablePatternMatcher {
     private final String pattern;
 
     /**
@@ -28,57 +24,11 @@ public class SingleLetterVariablePatternMatcher implements VariablePatternMatche
 
     @Override
     public boolean matches(String input) {
-        return matchRemaining(pattern, input, new HashMap<>()) != null;
+        return matchRemaining(pattern, input, new HashMap<>(), false) != null;
     }
 
     @Override
     public Map<String, String> getVariableAssignments(String input) {
-        return matchRemaining(pattern, input, new HashMap<>());
-    }
-
-    private Map<String, String> matchRemaining(String pattern, String input, Map<String, String> assignments) {
-        // If there is no more of the pattern left to match we need an empty input String to have matched the
-        // original input.
-        if (pattern.isEmpty()) {
-            if (input.isEmpty()) {
-                return assignments;
-            } else {
-                return null;
-            }
-        }
-
-        // Pick the first letter in the pattern as our current variable
-        final String variable = Character.toString(pattern.charAt(0));
-        // Count the number of times the variable occurs in the pattern
-        final int numberOfVariableOccurrencesInPattern = StringUtils.countMatches(pattern, variable);
-
-        // Greedily attempt to assign a value to the current variable
-        for (int endIndex = input.length(); endIndex > 0; endIndex--) {
-            final String variableAssignment = input.substring(0, endIndex);
-
-            // Check to make sure that the proper number of occurrences of the assigned value exist in the input
-            final int numberOfAssignmentOccurrencesInInput = StringUtils.countMatches(input, variableAssignment);
-            if (numberOfAssignmentOccurrencesInInput >= numberOfVariableOccurrencesInPattern) {
-                assignments.put(variable, variableAssignment);
-
-                // Get the permutations of removing the assignment the same number of times as the variable occurred
-                // in the input
-                final List<String> removalPermutations = StringRemover.getRemovalPermutations(variableAssignment,
-                        numberOfVariableOccurrencesInPattern, input);
-
-                // Check to see if the assignment leads to a valid match
-                for (String removalPermutation : removalPermutations) {
-                    final Map<String, String> result = matchRemaining(pattern.replaceAll(variable, ""),
-                            removalPermutation, assignments);
-                    if (result != null) {
-                        return result;
-                    }
-                }
-
-                assignments.remove(variable);
-            }
-        }
-
-        return null;
+        return matchRemaining(pattern, input, new HashMap<>(), false);
     }
 }
--- src/main/java/com/jmatch/matcher/StrictSingleLetterVariablePatternMatcher.java
+++ src/main/java/com/jmatch/matcher/StrictSingleLetterVariablePatternMatcher.java
@@ -1,10 +1,7 @@
 package com.jmatch.matcher;
 
 import com.google.common.collect.HashBiMap;
-import com.jmatch.util.StringRemover;
-import org.apache.commons.lang3.StringUtils;
 
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -14,11 +11,13 @@ import java.util.Map;
  * value. For example, the pattern {@code "xyyx"} would not match {@code "aaaa"} because both {@code 'x'} and {@code
  * 'y'} would have to be assigned the value {@code "a"}.
  */
-public class StrictSingleLetterVariablePatternMatcher implements VariablePatternMatcher {
+public class StrictSingleLetterVariablePatternMatcher extends LetterVariablePatternMatcher implements
+        VariablePatternMatcher {
     private final String pattern;
 
     /**
-     * Constructs a new strict matcher with the given pattern. The pattern should consist of single letters to represent
+     * Constructs a new strict matcher with the given pattern. The pattern should consist of single letters to
+     * represent
      * variables in the pattern. E.g. {@code "abba"} is composed of the variables {@code 'a'} and {@code 'b'}.
      *
      * @param pattern the given pattern
@@ -29,60 +28,13 @@ public class StrictSingleLetterVariablePatternMatcher implements VariablePattern
 
     @Override
     public boolean matches(String input) {
-        return matchRemaining(pattern, input, HashBiMap.create()) != null;
+        return matchRemaining(pattern, input, HashBiMap.create(), true) != null;
     }
 
     @Override
     public Map<String, String> getVariableAssignments(String input) {
-        return matchRemaining(pattern, input, HashBiMap.create());
+        return matchRemaining(pattern, input, HashBiMap.create(), true);
     }
 
-    private Map<String, String> matchRemaining(String pattern, String input, Map<String, String> assignments) {
-        // If there is no more of the pattern left to match we need an empty input String to have matched the
-        // original input.
-        if (pattern.isEmpty()) {
-            if (input.isEmpty()) {
-                return assignments;
-            } else {
-                return null;
-            }
-        }
 
-        // Pick the first letter in the pattern as our current variable
-        final String variable = Character.toString(pattern.charAt(0));
-        // Count the number of times the variable occurs in the pattern
-        final int numberOfVariableOccurrencesInPattern = StringUtils.countMatches(pattern, variable);
-
-        // Greedily attempt to assign a value to the current variable
-        for (int endIndex = input.length(); endIndex > 0; endIndex--) {
-            final String variableAssignment = input.substring(0, endIndex);
-
-            // Ensure that different variables do not have the same assigned value
-            if (assignments.containsValue(variableAssignment)) {
-                continue;
-            }
-
-            assignments.put(variable, variableAssignment);
-            // Check to make sure that the proper number of occurrences of the assigned value exist in the input
-            final int numberOfAssignmentOccurrencesInInput = StringUtils.countMatches(input, variableAssignment);
-            if (numberOfAssignmentOccurrencesInInput >= numberOfVariableOccurrencesInPattern) {
-                // Get the permutations of removing the assignment the same number of times as the variable occurred
-                // in the input
-                final List<String> removalPermutations = StringRemover.getRemovalPermutations(variableAssignment,
-                        numberOfVariableOccurrencesInPattern, input);
-
-                // Check to see if the assignment leads to a valid match
-                for (String removalPermutation : removalPermutations) {
-                    final Map<String, String> result = matchRemaining(pattern.replaceAll(variable, ""),
-                            removalPermutation, assignments);
-                    if (result != null) {
-                        return result;
-                    }
-                }
-            }
-            assignments.remove(variable);
-        }
-
-        return null;
-    }
 }
